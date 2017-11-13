6b24460eccc107e9acf08be3790052efb08a2f73
Todd Hunt <todd.hunt@mastercard.com>
2017-11-13 16:56:38 -0600

    add more cool stuff

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,34 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    public static final String PORT = "PORT";
+    public static final String MEMORY_LIMIT = "MEMORY_LIMIT";
+    public static final String CF_INSTANCE_INDEX = "CF_INSTANCE_INDEX";
+    public static final String CF_INSTANCE_ADDR = "CF_INSTANCE_ADDR";
+
+    private final Map<String, String> env = new HashMap<>();
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddr) {
+        env.put(PORT, port);
+        env.put(MEMORY_LIMIT, memoryLimit);
+        env.put(CF_INSTANCE_INDEX, cfInstanceIndex);
+        env.put(CF_INSTANCE_ADDR, cfInstanceAddr);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv(){
+        return env;
+    }
+}
